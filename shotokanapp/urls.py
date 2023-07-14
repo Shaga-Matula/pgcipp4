37471f6d07375f8de8da108ce3a052df4920e198
Paul-Gleeson <Shagamatula@gmail.com>
2023-07-14 14:36:46 +0000

    add class based views and welcome user on landing page

--- shotokanapp/urls.py
+++ shotokanapp/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
 from . import views
-
+from django.urls import path
 
 urlpatterns = [
-    path('', views.firstpage, name="home"),
+    path('', views.firstpage.as_view(), name="home"),
 ]
--- shotokanapp/views.py
+++ shotokanapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views import generic, View
 
 # Create your views here.
 
 
-def firstpage(request):
-    return render(request, 'index.html')
+class firstpage(View):
+
+    def get(self, request, *args, **kwargs):
+        context = {
+        'username': request.user.username,
+        }
+        return render(request, 'index.html', context)
