675005ecbe39daba34641ae26bf46d0f26151858
Paul-Gleeson <Shagamatula@gmail.com>
2023-07-17 13:37:37 +0000

    add user and kaya models to django admin

--- shotokanapp/admin.py
+++ shotokanapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student_info
+from .models import Student_Lvl
+
 
 # Register your models here.
+
+admin.site.register(Student_info)
+admin.site.register(Student_Lvl)
+
